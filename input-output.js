c02be0538bbebf1f38340f960a3d97f247e400d0
Max Yuri <maxyurisantos@hotmail.com>
2020-04-21 16:46:54 -0300

    Primeiro commite do código

---
+++ input-output.js
@@ -0,0 +1,6 @@
+
+var nome, idade;
+nome = prompt("Digite o seu nome:")
+idade = prompt("Digite a sua Idade:")
+alert("O nome digitado foi: " + nome)
+alert("A idade digitada foi: " + idade)
---
+++ operadores-relacionais.js
@@ -0,0 +1,25 @@
+/*
+ nota01, nota02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+     nota01 := 10
+     nota02 := 15
+     
+     se (nota01 >= nota02) entao
+         escreval ("nota01 é diferente")
+     senao
+         escreval ("Nota01 é igual")
+     fimse
+*/
+
+var nota01, nota02: real
+
+nota01 = 10
+nota02 = 15
+     
+     if (nota01 >= nota02) entao
+         alert ("nota01 é diferente")
+     else
+         alert ("Nota01 é igual")
+     
